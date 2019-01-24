44a0cbc2e356402faf15136d7e4f5b4415156089
TyLanger <ty.langer16@gmail.com>
2019-01-23 22:06:30 -0600

    Track Auto Curve Creation
    The track now automatically creates the curves instead of having to make
    them manualy.
    
    First, given transforms get turned into a list of points. A list because
    each curve will have a different number of new points being created that
    need to be inserted where the original corner point was.
    For each point, calculate the curve around it.
    The points that make up the curve get inserted into the list and the
    original corner is removed.
    The list is then converted to an array and passed into the even spacin
    function.
    This function was modified to take in an array of vector3's instead of
    using the array of transforms. It still operates on the evenPoints array
    directly instead of returning its own array.

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -8,21 +8,70 @@ public class Path : MonoBehaviour {
     int pointIndex = 0;
     Vector3[] evenPoints;
 
-    float pointSpacing = 2;
-    float railWidth = 1;
+    public float pointSpacing = 2;
+    public float railWidth = 1;
 
-    public Transform[] cornerTestTransforms;
-    Vector3[] cornerTestPoints;
     public float maxAngle = 45;
 
 	// Use this for initialization
 	void Awake () {
         evenPoints = new Vector3[1];
-        SpacePointsEqually();
+
+        // convert given transforms to a list of vector3
+        LinkedList<Vector3> trackPoints = new LinkedList<Vector3>();
+        foreach (var t in waypoints)
+        {
+            // add the position of each waypoint to the list
+            trackPoints.AddLast(t.position);
+        }
+        // create corners for each point
+        // track the relevent info for the corner. Need to know where it's coming from, where the corner is, and where the corner is going towards
+        LinkedListNode<Vector3> from = trackPoints.First;
+        LinkedListNode<Vector3> corner = from.Next;
+        LinkedListNode<Vector3> to = corner.Next;
+        for (int i = 1; i < waypoints.Length-1; i++)
+        {
+            // skip the first element and the last element because only interested in smoothing corners. The ends aren't corners
+            Vector3[] cornerPoints = SmoothCorner(from.Value, corner.Value, to.Value, maxAngle, pointSpacing);
+            //LinkedList<Vector3> cornerList = new LinkedList<Vector3>(cornerPoints);
+            foreach (var c in cornerPoints)
+            {
+                // insert each new corner point into the list
+                // insert before the to point so I never need to increment
+                // each subsequent point goes after the last one, but all before the to point
+                trackPoints.AddBefore(to, c);
+            }
+            // remove the point and replace it with the corner points
+            trackPoints.Remove(corner);
+            // increment to move to the next corner
+            // from is the last of the cornerPoints.
+            from = to.Previous;
+            corner = to;
+            to = to.Next;
+        }
+
+        // spacePointsEqually() using this list of points instead of waypoints transforms (need to refactor spacePointsEqually())
+        // create mesh
+        // when mesh is getting to big, try to split it into 2 meshes. 
+        // Split at 3 points that are all going in the same direction so split is seemless
+
+        // convert from a linked list to an array
+        Vector3[] trackPointsArray = new Vector3[trackPoints.Count];
+        LinkedListNode<Vector3> current = trackPoints.First;
+        for (int i = 0; i < trackPointsArray.Length; i++)
+        {
+            trackPointsArray[i] = current.Value;
+            current = current.Next;
+        }
+        // clear up memory
+        // well, shouldn't it be deleted when the function ends?
+        trackPoints.Clear();
+
+        SpacePointsEqually(trackPointsArray);
 
         GetComponent<MeshFilter>().mesh = CreateRailMesh();
 
-        cornerTestPoints = SmoothCorner(cornerTestTransforms[0].position, cornerTestTransforms[1].position, cornerTestTransforms[2].position, maxAngle, pointSpacing);
+        //cornerTestPoints = SmoothCorner(cornerTestTransforms[0].position, cornerTestTransforms[1].position, cornerTestTransforms[2].position, maxAngle, pointSpacing);
     }
 	
 	// Update is called once per frame
@@ -30,6 +79,8 @@ public class Path : MonoBehaviour {
 
 	}
 
+    // TOOD Rework for evenly spaced points
+    // May not have to do any checking. Just use the index of the current point * pointSpacing
     public float GetPathCompletion()
     {
         // returns how far along the path you are.
@@ -54,7 +105,7 @@ public class Path : MonoBehaviour {
         return currentPathDst / totalPathDst;
     }
 
-    void SpacePointsEqually()
+    void SpacePointsEqually(Vector3[] rawPoints)
     {
         // take the existing points, add points in between so that the points are all equal distance apart.
         // Might make the rail texture work better?
@@ -68,9 +119,9 @@ public class Path : MonoBehaviour {
         // along that vector, put in points every x spacing units
 
         float totalDistance = 0;
-        for (int i = 0; i < waypoints.Length-1; i++)
+        for (int i = 0; i < rawPoints.Length-1; i++)
         {
-            totalDistance += Vector3.Distance(waypoints[i].position, waypoints[i + 1].position);
+            totalDistance += Vector3.Distance(rawPoints[i], rawPoints[i + 1]);
         }
         //Debug.Log(totalDistance);
         Vector3[] points = new Vector3[(int)(totalDistance / pointSpacing)+2];
@@ -80,23 +131,23 @@ public class Path : MonoBehaviour {
         int pointsIndex = 0;
         Vector3 lastPoint;
         float overshoot = 0;
-        for (int i = 0; i < waypoints.Length-1; i++)
+        for (int i = 0; i < rawPoints.Length-1; i++)
         {
-            dir = (waypoints[i + 1].position - waypoints[i].position).normalized;
+            dir = (rawPoints[i + 1] - rawPoints[i]).normalized;
             //Debug.Log(dir);
             int index = 0;
             do
             {
-                lastPoint = waypoints[i].position + dir *(overshoot + pointSpacing * index);
+                lastPoint = rawPoints[i] + dir *(overshoot + pointSpacing * index);
                 points[pointsIndex] = lastPoint;
                 index++;
                 pointsIndex++;
 
                 // assume all points are always moving up
                 // therefore you can't have track that runs straight horizontal
-            } while (lastPoint.z < waypoints[i + 1].position.z);
+            } while (lastPoint.z < rawPoints[i + 1].z);
             // carry over how much you overshot the last point to the next one
-            overshoot = Vector3.Distance(lastPoint, waypoints[i + 1].position);
+            overshoot = Vector3.Distance(lastPoint, rawPoints[i + 1]);
             // remove the last point
             // so that the new points don't go past the original points
             pointsIndex--;
@@ -137,76 +188,85 @@ public class Path : MonoBehaviour {
         // how to round? truncate or round? If I truncate, I think sometimes I may end up with 1 too few segments
         // corner angle is negative if it's measuring counter clockwise
         int numNewPoints = Mathf.Abs(Mathf.RoundToInt(cornerAngle / maxAngle));
-        //Debug.Log("Num new points: " + numNewPoints);
-        // this is the real angle all the segments will be at
-        // should be close as close to maxAngle as they can get
-        float angle = cornerAngle / numNewPoints;
-        //Debug.Log("Angle: " + angle);
-        // there will be one fewer segments than the number of points
-        int numSegments = numNewPoints - 1;
-
-        Vector3[] cornerPoints = new Vector3[numNewPoints];
-
-        
-        // find the direction the points are going from fromPt to cornerPt
-        Vector3 currentDir = (cornerPt - fromPt).normalized;
-        //Debug.Log("Current Dir: " + currentDir);
-        cornerPoints[0] = Vector3.zero;
-
-        
-        for (int i = 1; i <= numSegments; i++)
+        if (numNewPoints > 0)
         {
-            // starting from the start,
-            // I want a new point segmentLength away at angle angle from the current vector
+            //Debug.Log("Num new points: " + numNewPoints);
+            // this is the real angle all the segments will be at
+            // should be close as close to maxAngle as they can get
+            float angle = cornerAngle / numNewPoints;
+            //Debug.Log("Angle: " + angle);
+            // there will be one fewer segments than the number of points
+            int numSegments = numNewPoints - 1;
 
-            // rotate current direction by angle
-            // 2D rotation
-            // x' = xCos() - zSin()
-            // z' = xSin() + zCos()
-            float x = currentDir.x;
-            float z = currentDir.z;
-            float sin = Mathf.Sin(-angle * Mathf.Deg2Rad);
-            float cos = Mathf.Cos(-angle * Mathf.Deg2Rad);
-            currentDir = new Vector3(x * cos - z * sin, currentDir.y, x * sin + z * cos).normalized;
+            Vector3[] cornerPoints = new Vector3[numNewPoints];
+
+
+            // find the direction the points are going from fromPt to cornerPt
+            Vector3 currentDir = (cornerPt - fromPt).normalized;
             //Debug.Log("Current Dir: " + currentDir);
-            // then the new point is
-            // points are added onto where the last one was
-            // all points are segmentLength apart
-            cornerPoints[i] = cornerPoints[i-1] + currentDir * segmentLength;
+            cornerPoints[0] = Vector3.zero;
+
+
+            for (int i = 1; i <= numSegments; i++)
+            {
+                // starting from the start,
+                // I want a new point segmentLength away at angle angle from the current vector
+
+                // rotate current direction by angle
+                // 2D rotation
+                // x' = xCos() - zSin()
+                // z' = xSin() + zCos()
+                float x = currentDir.x;
+                float z = currentDir.z;
+                float sin = Mathf.Sin(-angle * Mathf.Deg2Rad);
+                float cos = Mathf.Cos(-angle * Mathf.Deg2Rad);
+                currentDir = new Vector3(x * cos - z * sin, currentDir.y, x * sin + z * cos).normalized;
+                //Debug.Log("Current Dir: " + currentDir);
+                // then the new point is
+                // points are added onto where the last one was
+                // all points are segmentLength apart
+                cornerPoints[i] = cornerPoints[i - 1] + currentDir * segmentLength;
+            }
+
+            // figure out the distance from the first point to the last in a straight line
+            float dist = Vector3.Distance(cornerPoints[0], cornerPoints[cornerPoints.Length - 1]);
+
+            // solve the triangle made by the first point, the corner, and the last point
+            // Made a right triangle by taking the midpoint between the first point and last point
+            // Could have done it with the whole triangle, but I did it this way first
+            // TODO: don't cut the triangle in half
+            // lowercase are side lengths
+            // uppercase are angles
+            // a is half the length from point[0] to point[length-1] or half the measured dist
+            // in the figure above, cornerAngle is A. We want B from the figure above so sub 180 and then half because we're using half the triangle
+            // C is 90 because it's a right triangle
+            float a = dist / 2;
+            float A = (180 - cornerAngle) / 2;
+            float C = 90;
+            //Debug.LogFormat("a: {0}, A: {1}, B: {2}, C: {3}", a, A, A, C);
+
+            // calculate the side length using Sine Law
+            float distFromCorner = (a * Mathf.Sin(C * Mathf.Deg2Rad)) / Mathf.Sin(A * Mathf.Deg2Rad);
+            //Debug.Log("Dist from Corner: " + distFromCorner);
+
+            // dist from the corner tells us how far away from the corner the end points are (it's the same for either end point
+            // using the vector from the corner to fromPt, place the first point distFromCorner distance in that direction
+            cornerPoints[0] = cornerPt + distFromCorner * (fromPt - cornerPt).normalized;
+            // all the points are oriented in relation to the first point.
+            // Just add the first point's new position to their position and it translates their positions to world coordinates.
+            for (int i = 1; i <= numSegments; i++)
+            {
+                cornerPoints[i] += cornerPoints[0];
+            }
+
+            return cornerPoints;
         }
-
-        // figure out the distance from the first point to the last in a straight line
-        float dist = Vector3.Distance(cornerPoints[0], cornerPoints[cornerPoints.Length - 1]);
-
-        // solve the triangle made by the first point, the corner, and the last point
-        // Made a right triangle by taking the midpoint between the first point and last point
-        // Could have done it with the whole triangle, but I did it this way first
-        // TODO: don't cut the triangle in half
-        // lowercase are side lengths
-        // uppercase are angles
-        // a is half the length from point[0] to point[length-1] or half the measured dist
-        // in the figure above, cornerAngle is A. We want B from the figure above so sub 180 and then half because we're using half the triangle
-        // C is 90 because it's a right triangle
-        float a = dist/2;
-        float A = (180-cornerAngle)/2;
-        float C = 90;
-        //Debug.LogFormat("a: {0}, A: {1}, B: {2}, C: {3}", a, A, A, C);
-
-        // calculate the side length using Sine Law
-        float distFromCorner = (a * Mathf.Sin(C * Mathf.Deg2Rad)) / Mathf.Sin(A * Mathf.Deg2Rad);
-        //Debug.Log("Dist from Corner: " + distFromCorner);
-
-        // dist from the corner tells us how far away from the corner the end points are (it's the same for either end point
-        // using the vector from the corner to fromPt, place the first point distFromCorner distance in that direction
-        cornerPoints[0] = cornerPt + distFromCorner * (fromPt - cornerPt).normalized;
-        // all the points are oriented in relation to the first point.
-        // Just add the first point's new position to their position and it translates their positions to world coordinates.
-        for (int i = 1; i <= numSegments; i++)
+        else
         {
-            cornerPoints[i] += cornerPoints[0];
+            Vector3[] singlePtArray = new Vector3[1];
+            singlePtArray[0] = cornerPt;
+            return singlePtArray;
         }
-
-        return cornerPoints;
     }
 
     Mesh CreateRailMesh()
@@ -373,6 +433,7 @@ public class Path : MonoBehaviour {
             }
         }
 
+        /*
         Gizmos.color = Color.white;
         // Corner Test
         foreach (var t in cornerTestTransforms)
@@ -400,6 +461,18 @@ public class Path : MonoBehaviour {
             }
         }
 
+        if(trackPoints != null)
+        {
+            if(trackPoints.Count > 0)
+            {
+                Gizmos.color = Color.blue;
+                foreach (var tp in trackPoints)
+                {
+                    Gizmos.DrawWireSphere(tp, 0.075f);
+                }
+            }
+        }
+        */
     }
     
 }
