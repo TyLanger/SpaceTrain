afccf3d6e31b876d52cd2c861a2ad6e0f23f1c00
TyLanger <ty.langer16@gmail.com>
2019-03-08 17:43:23 -0600

    Boarding Links and Boarding Lookahead
    BoardingLink
    Class to store the link between the ground position and position on the
    train. Enemies will be able to go to the ground position to be able to
    board the train.
    Currently just calculates the position the ground point will be at given a
    certain train location and rotation
    
    Train
    Made GetBoardingLocationsInTime(t) functional.
    Calculates where the train will be in time t and at what rotation.
    It then passes this information to the attached BoardingLinks to find
    their ground positions.
    Then it returns these ground positions for the enemy to use them to plan a
    path.
    
    Problems:
    The lookahead doesn't work properly until the front and back wheels of the
    train are on the track.
    Each train calls its own links. An enemy would therefore need to check
    each train for the links. This may or may not be a bad thing.

---
+++ Assets/Scripts/BoardingLink.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardingLink : MonoBehaviour {
+
+    public Transform trainPoint;
+    public Transform groundPoint;
+
+    Train train;
+    Vector3 trainOffset;
+    Vector3 groundOffset;
+    //Vector3 onboardOffset;
+
+
+
+    // Debugging
+    Vector3 trainPointLookahead;
+    Vector3 groundPointLookahead;
+
+    [Range(0.1f, 0.8f)]
+    public float drawRadius = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        train = GetComponentInParent<Train>();
+        if(train == null)
+        {
+            Debug.Log("Boarding Link has no train parent");
+        }
+        // is storing the offset of the transform to the train AND the transform to the ground child redundant? probably
+        // could probably just store offset of train to ground point
+        trainOffset = train.transform.position - transform.position;
+        groundOffset = groundPoint.position - transform.position;
+        //onboardOffset = trainPoint.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// update the height of the ground point to remain attached to the ground?
+	}
+
+    public Vector3 GroundPointAtPosition(Vector3 trainPos, float trainAngle)
+    {
+        // given a train position and angle,
+        // where will the ground point be?
+
+        // find the positon by rotating the offset (calcuated in start) and adding to the train pos
+        Vector3 myPos = trainPos - new Vector3(trainOffset.x * Mathf.Cos(trainAngle) - trainOffset.z * Mathf.Sin(trainAngle), 0, trainOffset.x * Mathf.Sin(trainAngle) + trainOffset.z * Mathf.Cos(trainAngle));
+        Vector3 groundPos = myPos + new Vector3(groundOffset.x * Mathf.Cos(trainAngle) - groundOffset.z * Mathf.Sin(trainAngle), 0, groundOffset.x * Mathf.Sin(trainAngle) + groundOffset.z * Mathf.Cos(trainAngle));
+
+        // for OnDrawGizmos
+        //groundPointLookahead = groundPos; 
+
+        return groundPos; 
+    }
+
+    void OnDrawGizmos()
+    {
+        // train point is green
+        Gizmos.color = Color.green;
+        if(trainPoint != null)
+            Gizmos.DrawWireSphere(trainPoint.position, drawRadius);
+
+        // ground point is magenta
+        Gizmos.color = Color.magenta;
+        if(groundPoint != null)
+            Gizmos.DrawWireSphere(groundPoint.position, drawRadius);
+
+        Gizmos.color = Color.cyan;
+        if(groundPoint != null && trainPoint != null)
+            Gizmos.DrawLine(trainPoint.position, groundPoint.position);
+
+
+        Gizmos.color = Color.blue;
+        if (groundPointLookahead != Vector3.zero)
+            Gizmos.DrawWireSphere(groundPointLookahead, drawRadius);
+    }
+}
--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -20,15 +20,54 @@ public class Train : MonoBehaviour {
     Vector3 rearPathTarget;
     int frontWheelIndex = 0;
     int rearWheelIndex = 0;
+    //Vector3 frontWheelOffset;
+    float frontWheelDistanceOffset;
+    float frontWheelHeightOffset;
 
+    // Boarding
+    public BoardingLink[] leftLinks;
+    public BoardingLink[] rightLinks;
 
+    // Boarding Testing
+    float testTime = 4;
 
-	// Use this for initialization
-	void Start () {
+    // Gizmos
+    Vector3 newTrainPos;
+    Vector3 newFrontPos;
+    Vector3 newEndPos;
+
+    // Use this for initialization
+    void Start () {
         currentSpeed = baseSpeed;
         pathTarget = path.GetFirstPoint();
         rearPathTarget = pathTarget;
-	}
+        //frontWheelOffset = transform.position - frontWheels.position;
+        // check the distance between the front wheel and the train's transform
+        // check vertical distance (y) and horizontal distance (x and z)
+        frontWheelHeightOffset = transform.position.y - frontWheels.position.y;
+        frontWheelDistanceOffset = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(frontWheels.position.x, frontWheels.position.z));
+
+        // ~15 seconds for all 3 cars to be on the track
+        //Invoke("TestBoarding", 15);
+    }
+
+    void TestBoarding()
+    {
+        //testing
+        Vector3[] testPoints = GetBoardingLocationsInTime(testTime);
+        
+        foreach (var item in testPoints)
+        {
+            Debug.Log("Forward Position: " + item);
+        }
+        Invoke("Pause", testTime);
+        
+    }
+
+    void Pause()
+    {
+        Debug.Break();
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -91,27 +130,61 @@ public class Train : MonoBehaviour {
     public Vector3[] GetBoardingLocationsInTime(float t, bool rightSide = true)
     {
         // train will have some spots on them for jumping aboard
-        // the number of them will be created elswhere later on
-        // just like this right now to suppress errors
-        int numBoardingSpots = 6;
-        Vector3[] boardingSpots = new Vector3[numBoardingSpots];
+        // should be rightLinks.Length + leftLinks.Length
+        // but I only have right links at the moment
+        Vector3[] boardingSpots = new Vector3[rightLinks.Length];
 
         float totalDist = currentSpeed * (t / Time.fixedDeltaTime);
         // can't use Train.PositionInTime() because it only uses the frontWheelIndex
         // this method should largely replace that one anyway
         Vector3 frontPt = path.GetPointInDistance(frontWheelIndex, totalDist);
         Vector3 rearPt = path.GetPointInDistance(rearWheelIndex, totalDist);
+        // this ^ will fail in the first few seconds of the game. Both the front and rear wheels start by moving towards the first point in the path (index 0)
+        // index is incremented when they reach a point. The front wheel will reach points and start incrementing soon after starting
+        // the rear wheels take some time to reach the first point. 
+        // As such, they won't be at their "real" index difference (about 15) for a little while. front wheel will be at 0,1,2,3,4,5,6,7... while rear is still at 0
+        // until about front 16, where rear will finally get to 1
+        // this messes up the lookahead because you will be looking for 2 points 6 indicies apart instead of the proper 15
+        // the problem is even worse for additional train cars. They take even longer to get on the track (~15 seconds with the current 3 cars)
 
         Vector3 trainForward = frontPt - rearPt;
-        float angle = Vector3.Angle(Vector3.forward, trainForward);
+        float angle = Vector3.Angle(Vector3.forward, trainForward) * Mathf.Deg2Rad * -1;
         // due to rounding in path.GetPointInDistance()
         // all these positions will be approximate
+        // path.GetPointInDistance() returns a location exactly on one of its points. It won't return a location between points.
+        // front and rear wheels of the train also don't sit exactly on a point at the same time because they aren't a whole number of points apart.
+        // Even if they were, that wouldn't hold true around corners.
 
-        // rotate the boarding spots.... somehow
+
+        // I thought the bug was in this rotating the train too much
+        // but the bug was the rearpt being in the worng spot.
+        // so rotating the the front wheel offset might work
+        // rotate offset
         // x' = xcos() - ysin()
         // y' = xSin() + yCos()
-        // should work I think
-        // careful converting from local space to world space
+        //Vector3 rotatedFrontWheelOffset = new Vector3(frontWheelOffset.x * Mathf.Cos(angle) - frontWheelOffset.z * Mathf.Sin(angle), frontWheelOffset.y, frontWheelOffset.x * Mathf.Sin(angle) + frontWheelOffset.z * Mathf.Cos(angle));
+        //Vector3 trainPos = frontPt + rotatedFrontWheelOffset;
+
+        // train position will be along the line from the back wheels to the front wheels. 
+        // the train position is a distance from the front wheels and a certain height above them
+        // these offsets are stored in frontWheelDistanceOffset and frontWheelHeightOffset
+        Vector3 trainPos = frontPt - trainForward.normalized * frontWheelDistanceOffset + new Vector3(0, frontWheelHeightOffset, 0);  
+
+        /*
+        //testing
+        newTrainPos = trainPos;
+        newFrontPos = frontPt;
+        newEndPos = rearPt;
+        //end
+        */
+
+        // check what the orientation of all the boarding points will be at the given time.
+        // each link knows its position in relation to the train. Giving them the train's location and rotation at the given time,
+        // they can calculate where they will be.
+        for (int i = 0; i < rightLinks.Length; i++)
+        {
+            boardingSpots[i] = rightLinks[i].GroundPointAtPosition(trainPos, angle);
+        }
 
         // order the points front to back left side, then front to back rights side?
         // or have a separate method of computing left and right side?
@@ -124,5 +197,27 @@ public class Train : MonoBehaviour {
     void ReachedEndOfLine()
     {
         currentSpeed = 0;
+    }
+
+    void OnDrawGizmos()
+    {
+        /*
+        if (rightLinks.Length > 0)
+        {
+            Gizmos.color = Color.magenta;
+            if (newTrainPos != Vector3.zero)
+            {
+                Gizmos.DrawWireSphere(newTrainPos, 0.5f);
+                
+            }
+
+            if (newFrontPos != Vector3.zero)
+            {
+                Gizmos.DrawWireSphere(newFrontPos, 0.3f);
+                Gizmos.DrawWireSphere(newEndPos, 0.3f);
+                Gizmos.DrawLine(newFrontPos, newEndPos);
+            }
+        }
+        */
     }
 }
